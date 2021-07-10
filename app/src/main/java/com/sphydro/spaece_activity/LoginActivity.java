e31407c3768ad8f03d552b3a099363a9d3f27dd1
Nikhil Paleti <paletinikhil@gmail.com>
2021-07-11 00:17:24 +0530

    Login and Signup Activities can switch Now
    You can switch between SignUp and Login as we typically expect to do.
    Also made minor changes in manifest file for HTTPS

--- app/src/main/java/com/sphydro/spaece_activity/LoginActivity.java
+++ app/src/main/java/com/sphydro/spaece_activity/LoginActivity.java
@@ -2,13 +2,36 @@ package com.sphydro.spaece_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText username, password;
+    Button login;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.textInputEditText2);
+        password = findViewById(R.id.textInputEditText5);
+        login = (Button) findViewById(R.id.login_button);
+        tv = findViewById(R.id.switch_signup_button);
+
+        tv.setOnClickListener(v -> {
+            openRegister();
+            });
+        }
+
+    private void openRegister() {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
     }
 }
--- app/src/main/java/com/sphydro/spaece_activity/SignupActivity.java
+++ app/src/main/java/com/sphydro/spaece_activity/SignupActivity.java
@@ -2,13 +2,39 @@ package com.sphydro.spaece_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    TextInputEditText name, username, email, phone, password;
+    Button signup;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.textInputEditText);
+        username = findViewById(R.id.textInputEditText2);
+        email = findViewById(R.id.textInputEditText3);
+        phone = findViewById(R.id.textInputEditText4);
+        password = findViewById(R.id.textInputEditText5);
+        signup = (Button) findViewById(R.id.button_register);
+        tv = findViewById(R.id.switch_login_button);
+
+        tv.setOnClickListener(v -> {
+            openLogin();
+            });
+    }
+
+    private void openLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
